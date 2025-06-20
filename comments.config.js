631246fa22ddd1ee5deab0a4a811f1b805e8c817
Luther Tchofo Safo <nintendobserver@gmail.com>
2025-06-20 12:05:58 +0200

    data and ignores in comments.config.js now.

--- comments.config.js
+++ comments.config.js
@@ -1,4 +1,4 @@
-const config = {
+const data = {
   jsDoc: {
     getSourceCodeFromFilePath: `Gets the ESLint-generated SourceCode object of a file from its absolute path.
  * @param {string} absolutePath The absolute path of the file.
@@ -6,6 +6,13 @@ const config = {
  * @param {import('eslint').Linter} linter The Linter instance used to retrieve the SourceCode object, defaulting to a new Linter per operation,  ensuring each instance of the function is based on its own linter (just in case somehow some linters were to run concurrently).
  * @returns The ESLint-generated SourceCode object of the file, from which the AST (\`sourceCode.ast\`) and all comments (\`sourceCode.getAllComments()\`) can be extracted.`,
   },
+};
+
+const ignores = [];
+
+const config = {
+  data,
+  ignores,
 };
 
 export default config;
