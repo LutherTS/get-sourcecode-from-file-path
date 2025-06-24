f98ba1bb8d24728e7be3ffcb1dd602f587193182
Luther Tchofo Safo <nintendobserver@gmail.com>
2025-06-24 16:11:44 +0200

    About to compress for getSourceCodeFromFilePath.

--- comments.config.js
+++ comments.config.js
@@ -4,7 +4,23 @@ const data = {
  * @param {string} absolutePath The absolute path of the file.
  * @param {import('eslint').Linter.LanguageOptions} languageOptions The languageOptions object used by \`linter.verify()\`, defaulting to a version that is TypeScript- and JSX-compatible.
  * @param {import('eslint').Linter} linter The Linter instance used to retrieve the SourceCode object, defaulting to a new Linter per operation,  ensuring each instance of the function is based on its own linter (just in case somehow some linters were to run concurrently).
- * @returns The ESLint-generated SourceCode object of the file, from which the AST (\`sourceCode.ast\`) and all comments (\`sourceCode.getAllComments()\`) can be extracted.`,
+ * @returns The ESLint-generated SourceCode object of the file, from which the AST (\`sourceCode.ast\`) and all comments (\`sourceCode.getAllComments()\`) can be extracted.`, // $COMMENT#JSDOC#GETSOURCECODEFROMFILEPATH
+    definitions: {
+      getSourceCodeFromFilePath:
+        "Gets the ESLint-generated SourceCode object of a file from its absolute path.",
+    }, // $COMMENT#JSDOC#DEFINITIONS#GETSOURCECODEFROMFILEPATH
+    params: {
+      absolutePath: "The absolute path of the file.", // $COMMENT#JSDOC#PARAMS#ABSOLUTEPATH
+      options: "The additional options as follows:", // $COMMENT#JSDOC#PARAMS#OPTIONS
+      languageOptionsOption:
+        "The languageOptions object used by `linter.verify()`, defaulting to a version that is TypeScript- and JSX-compatible.", // $COMMENT#JSDOC#PARAMS#LANGUAGEOPTIONSOPTION
+      linterOption:
+        "The Linter instance used to retrieve the SourceCode object, defaulting to a `new Linter()` per operation, ensuring each instance of the function is based on its own linter (just in case somehow some linters were to run concurrently).", // $COMMENT#JSDOC#PARAMS#LINTEROPTION
+    },
+    returns: {
+      getSourceCodeFromFilePath:
+        "The ESLint-generated SourceCode object of the file, from which the AST (`sourceCode.ast`) and all comments (`sourceCode.getAllComments()`) can be extracted.", // $COMMENT#JSDOC#RETURNS#GETSOURCECODEFROMFILEPATH
+    },
   },
 };
 
--- index.js
+++ index.js
@@ -20,13 +20,18 @@ const typeScriptAndJSXCompatible = {
  * @param {string} absolutePath The absolute path of the file.
  * @param {import('eslint').Linter.LanguageOptions} languageOptions The languageOptions object used by `linter.verify()`, defaulting to a version that is TypeScript- and JSX-compatible.
  * @param {import('eslint').Linter} linter The Linter instance used to retrieve the SourceCode object, defaulting to a new Linter per operation,  ensuring each instance of the function is based on its own linter (just in case somehow some linters were to run concurrently).
+ * @param {{languageOptions: import('eslint').Linter.LanguageOptions; linter: import('eslint').Linter}} options
  * @returns The ESLint-generated SourceCode object of the file, from which the AST (`sourceCode.ast`) and all comments (`sourceCode.getAllComments()`) can be extracted.
  */
 export const getSourceCodeFromFilePath = (
   absolutePath,
-  languageOptions = typeScriptAndJSXCompatible,
-  linter = new Linter()
+  options = {
+    languageOptions: typeScriptAndJSXCompatible,
+    linter: new Linter(),
+  }
 ) => {
+  const { languageOptions, linter } = options;
+
   // the raw code of the file at the end of the absolute path
   const text = fs.readFileSync(absolutePath, "utf8");
   // utilizes linter.verify ...
