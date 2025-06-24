798bdd2bdbedb2b64823d329d93a16e3236777f1
Luther Tchofo Safo <nintendobserver@gmail.com>
2025-06-24 17:08:17 +0200

    Now with options object.

--- comments.config.js
+++ comments.config.js
@@ -1,10 +1,5 @@
 const data = {
   jsDoc: {
-    getSourceCodeFromFilePath: `Gets the ESLint-generated SourceCode object of a file from its absolute path.
- * @param {string} absolutePath The absolute path of the file.
- * @param {import('eslint').Linter.LanguageOptions} languageOptions The languageOptions object used by \`linter.verify()\`, defaulting to a version that is TypeScript- and JSX-compatible.
- * @param {import('eslint').Linter} linter The Linter instance used to retrieve the SourceCode object, defaulting to a new Linter per operation,  ensuring each instance of the function is based on its own linter (just in case somehow some linters were to run concurrently).
- * @returns The ESLint-generated SourceCode object of the file, from which the AST (\`sourceCode.ast\`) and all comments (\`sourceCode.getAllComments()\`) can be extracted.`, // $COMMENT#JSDOC#GETSOURCECODEFROMFILEPATH
     definitions: {
       getSourceCodeFromFilePath:
         "Gets the ESLint-generated SourceCode object of a file from its absolute path.",
--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const typeScriptAndJSXCompatible = {
 /**
  * Gets the ESLint-generated SourceCode object of a file from its absolute path.
  * @param {string} absolutePath The absolute path of the file.
- * @param {Object} options The additional options as follows:
+ * @param {Object} [options] The additional options as follows:
  * @param {import('eslint').Linter} [options.linter] The Linter instance used to retrieve the SourceCode object, defaulting to a `new Linter()` per operation, ensuring each instance of the function is based on its own linter (just in case somehow some linters were to run concurrently).
  * @param {import('eslint').Linter.LanguageOptions} [options.languageOptions] The languageOptions object used by `linter.verify()`, defaulting to a version that is TypeScript- and JSX-compatible.
  * @returns The ESLint-generated SourceCode object of the file, from which the AST (`sourceCode.ast`) and all comments (`sourceCode.getAllComments()`) can be extracted.
