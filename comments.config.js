4c5262d49285242e9ff3bcd1da120ade17255f84
Luther Tchofo Safo <nintendobserver@gmail.com>
2025-06-24 17:05:02 +0200

    About to compress again.

--- comments.config.js
+++ comments.config.js
@@ -12,10 +12,10 @@ const data = {
     params: {
       absolutePath: "The absolute path of the file.", // $COMMENT#JSDOC#PARAMS#ABSOLUTEPATH
       options: "The additional options as follows:", // $COMMENT#JSDOC#PARAMS#OPTIONS
-      languageOptionsOption:
-        "The languageOptions object used by `linter.verify()`, defaulting to a version that is TypeScript- and JSX-compatible.", // $COMMENT#JSDOC#PARAMS#LANGUAGEOPTIONSOPTION
       linterOption:
         "The Linter instance used to retrieve the SourceCode object, defaulting to a `new Linter()` per operation, ensuring each instance of the function is based on its own linter (just in case somehow some linters were to run concurrently).", // $COMMENT#JSDOC#PARAMS#LINTEROPTION
+      languageOptionsOption:
+        "The languageOptions object used by `linter.verify()`, defaulting to a version that is TypeScript- and JSX-compatible.", // $COMMENT#JSDOC#PARAMS#LANGUAGEOPTIONSOPTION
     },
     returns: {
       getSourceCodeFromFilePath:
--- index.js
+++ index.js
@@ -18,20 +18,15 @@ const typeScriptAndJSXCompatible = {
 /**
  * Gets the ESLint-generated SourceCode object of a file from its absolute path.
  * @param {string} absolutePath The absolute path of the file.
- * @param {import('eslint').Linter.LanguageOptions} languageOptions The languageOptions object used by `linter.verify()`, defaulting to a version that is TypeScript- and JSX-compatible.
- * @param {import('eslint').Linter} linter The Linter instance used to retrieve the SourceCode object, defaulting to a new Linter per operation,  ensuring each instance of the function is based on its own linter (just in case somehow some linters were to run concurrently).
- * @param {{languageOptions: import('eslint').Linter.LanguageOptions; linter: import('eslint').Linter}} options
+ * @param {Object} options The additional options as follows:
+ * @param {import('eslint').Linter} [options.linter] The Linter instance used to retrieve the SourceCode object, defaulting to a `new Linter()` per operation, ensuring each instance of the function is based on its own linter (just in case somehow some linters were to run concurrently).
+ * @param {import('eslint').Linter.LanguageOptions} [options.languageOptions] The languageOptions object used by `linter.verify()`, defaulting to a version that is TypeScript- and JSX-compatible.
  * @returns The ESLint-generated SourceCode object of the file, from which the AST (`sourceCode.ast`) and all comments (`sourceCode.getAllComments()`) can be extracted.
  */
 export const getSourceCodeFromFilePath = (
   absolutePath,
-  options = {
-    languageOptions: typeScriptAndJSXCompatible,
-    linter: new Linter(),
-  }
+  { languageOptions = typeScriptAndJSXCompatible, linter = new Linter() } = {}
 ) => {
-  const { languageOptions, linter } = options;
-
   // the raw code of the file at the end of the absolute path
   const text = fs.readFileSync(absolutePath, "utf8");
   // utilizes linter.verify ...
