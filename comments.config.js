92a8afe37c504da70b598b46974c04659127db17
Luther Tchofo Safo <nintendobserver@gmail.com>
2025-06-25 02:38:25 +0200

    Object.freeze in config.

--- comments.config.js
+++ comments.config.js
@@ -1,22 +1,22 @@
 const data = {
-  jsDoc: {
-    definitions: {
+  jsDoc: Object.freeze({
+    definitions: Object.freeze({
       getSourceCodeFromFilePath:
         "Gets the ESLint-generated SourceCode object of a file from its absolute path.",
-    }, // $COMMENT#JSDOC#DEFINITIONS#GETSOURCECODEFROMFILEPATH
-    params: {
+    }), // $COMMENT#JSDOC#DEFINITIONS#GETSOURCECODEFROMFILEPATH
+    params: Object.freeze({
       absolutePath: "The absolute path of the file.", // $COMMENT#JSDOC#PARAMS#ABSOLUTEPATH
       options: "The additional options as follows:", // $COMMENT#JSDOC#PARAMS#OPTIONS
       linterOption:
         "The Linter instance used to retrieve the SourceCode object, defaulting to a `new Linter()` per operation, ensuring each instance of the function is based on its own linter (just in case somehow some linters were to run concurrently).", // $COMMENT#JSDOC#PARAMS#LINTEROPTION
       languageOptionsOption:
         "The languageOptions object used by `linter.verify()`, defaulting to a version that is TypeScript- and JSX-compatible.", // $COMMENT#JSDOC#PARAMS#LANGUAGEOPTIONSOPTION
-    },
-    returns: {
+    }),
+    returns: Object.freeze({
       getSourceCodeFromFilePath:
         "The ESLint-generated SourceCode object of the file, from which the AST (`sourceCode.ast`) and all comments (`sourceCode.getAllComments()`) can be extracted.", // $COMMENT#JSDOC#RETURNS#GETSOURCECODEFROMFILEPATH
-    },
-  },
+    }),
+  }),
 };
 
 const ignores = [];
