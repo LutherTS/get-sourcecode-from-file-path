829da85154e5873edb28797cd75defeab71b2f53
Luther Tchofo Safo <nintendobserver@gmail.com>
2026-01-08 08:41:19 +0100

    Exposing null from the return.

--- README.md
+++ README.md
@@ -2,12 +2,12 @@ Another utility that I am bound to reuse between open-source projects. (I'll con
 
 ```js
 /**
- * Gets the ESLint-generated SourceCode object of a file from its absolute path.
- * @param {string} absolutePath The absolute path of the file.
- * @param {Object} [options] The additional options as follows:
- * @param {import('eslint').Linter} [options.linter] The Linter instance used to retrieve the SourceCode object, defaulting to a `new Linter()` per operation, ensuring each instance of the function is based on its own linter (just in case somehow some linters were to run concurrently).
- * @param {import('eslint').Linter.LanguageOptions} [options.languageOptions] The languageOptions object used by `linter.verify()`, defaulting to a version that is TypeScript- and JSX-compatible.
- * @returns The ESLint-generated SourceCode object of the file, from which the AST (`sourceCode.ast`) and all comments (`sourceCode.getAllComments()`) can be extracted.
+ * $COMMENT#JSDOC#DEFINITIONS#GETSOURCECODEFROMFILEPATH
+ * @param {string} absolutePath $COMMENT#JSDOC#PARAMS#ABSOLUTEPATH
+ * @param {Object} [options] $COMMENT#JSDOC#PARAMS#OPTIONS
+ * @param {import('eslint').Linter} [options.linter] $COMMENT#JSDOC#PARAMS#LINTEROPTION
+ * @param {import('eslint').Linter.LanguageOptions} [options.languageOptions] $COMMENT#JSDOC#PARAMS#LANGUAGEOPTIONSOPTION
+ * @returns $COMMENT#JSDOC#RETURNS#GETSOURCECODEFROMFILEPATH
  */
 export const getSourceCodeFromFilePath = (
   absolutePath,
--- comments.config.js
+++ comments.config.js
@@ -2,19 +2,21 @@ const data = {
   jsDoc: Object.freeze({
     definitions: Object.freeze({
       getSourceCodeFromFilePath:
-        "Gets the ESLint-generated SourceCode object of a file from its absolute path.",
-    }), // $COMMENT#JSDOC#DEFINITIONS#GETSOURCECODEFROMFILEPATH
+        "Gets the ESLint-generated `SourceCode` object of a file from its absolute path." /* $COMMENT#JSDOC#DEFINITIONS#GETSOURCECODEFROMFILEPATH */,
+    }),
     params: Object.freeze({
-      absolutePath: "The absolute path of the file.", // $COMMENT#JSDOC#PARAMS#ABSOLUTEPATH
-      options: "The additional options as follows:", // $COMMENT#JSDOC#PARAMS#OPTIONS
+      absolutePath:
+        "The absolute path of the file." /* $COMMENT#JSDOC#PARAMS#ABSOLUTEPATH */,
+      options:
+        "The additional options as follows:" /* $COMMENT#JSDOC#PARAMS#OPTIONS */,
       linterOption:
-        "The Linter instance used to retrieve the SourceCode object, defaulting to a `new Linter()` per operation, ensuring each instance of the function is based on its own linter (just in case somehow some linters were to run concurrently).", // $COMMENT#JSDOC#PARAMS#LINTEROPTION
+        "The Linter instance used to retrieve the `SourceCode` object, defaulting to a `new Linter()` per operation, ensuring each instance of the function is based on its own linter (just in case somehow some linters were to run concurrently)." /* $COMMENT#JSDOC#PARAMS#LINTEROPTION */,
       languageOptionsOption:
-        "The languageOptions object used by `linter.verify()`, defaulting to a version that is TypeScript- and JSX-compatible.", // $COMMENT#JSDOC#PARAMS#LANGUAGEOPTIONSOPTION
+        "The `languageOptions` object used by `linter.verify()`, defaulting to a version that is TypeScript- and JSX-compatible." /* $COMMENT#JSDOC#PARAMS#LANGUAGEOPTIONSOPTION */,
     }),
     returns: Object.freeze({
       getSourceCodeFromFilePath:
-        "The ESLint-generated SourceCode object of the file, from which the AST (`sourceCode.ast`) and all comments (`sourceCode.getAllComments()`) can be extracted.", // $COMMENT#JSDOC#RETURNS#GETSOURCECODEFROMFILEPATH
+        "The ESLint-generated `SourceCode` object of the file, from which the AST (`sourceCode.ast`) and all comments (`sourceCode.getAllComments()`) can be extracted. `null` when encountering fatal syntax." /* $COMMENT#JSDOC#RETURNS#GETSOURCECODEFROMFILEPATH */,
     }),
   }),
 };
