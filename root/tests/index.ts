1aec5d972256711b6d17be4da852ef0e9a2efcf8
Luther Tchofo Safo <nintendobserver@gmail.com>
2026-05-03 14:31:03 +0200

    Resolved for prod.

--- root/tests/index.ts
+++ root/tests/index.ts
@@ -39,8 +39,8 @@ const languages = [
 }[];
 
 /**
- * $COMMENT#JSDOC#TESTS#DEFS#ASSERTNULLPATH
- * @param filePath $COMMENT#JSDOC#TESTS#PARAMS#FILEPATH
+ * Asserts that the file path passed to `getSourceCodeFromFilePath` correctly returns `null` when the file is nonexistent or when encountering fatal syntax. (The assertion process happens within this utility.)
+ * @param filePath The file path at hand.
  */
 const assertNullPath = (filePath: string) => {
   const results = getSourceCodeFromFilePath(filePath);
@@ -48,8 +48,8 @@ const assertNullPath = (filePath: string) => {
 };
 
 /**
- * $COMMENT#JSDOC#TESTS#DEFS#ASSERTFILEPATH
- * @param filePath $COMMENT#JSDOC#TESTS#PARAMS#FILEPATH
+ * Asserts that the file path passed to `getSourceCodeFromFilePath` correctly returns a `SourceCode` object when the file exists and is valid. (The assertion process happens within this utility.)
+ * @param filePath The file path at hand.
  */
 const assertFilePath = (filePath: string) => {
   const results = getSourceCodeFromFilePath(filePath);
