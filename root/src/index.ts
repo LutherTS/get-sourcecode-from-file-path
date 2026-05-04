4c7658f541dfd111c5e2c8d82e605b789f246aa5
Luther Tchofo Safo <nintendobserver@gmail.com>
2026-05-04 03:01:10 +0200

    Resolved for prod. (With Notes.)

--- root/src/index.ts
+++ root/src/index.ts
@@ -13,12 +13,12 @@ const typeScriptAndJSXCompatible = {
 } as const satisfies Linter.LanguageOptions;
 
 /**
- * $COMMENT#JSDOC#CORE#DEFS#GETSOURCECODEFROMFILEPATH
- * @param absolutePath - $COMMENT#JSDOC#CORE#PARAMS#ABSOLUTEPATH
- * @param options - $COMMENT#JSDOC#CORE#PARAMS#OPTIONSPARAM
- * @param options.linter - $COMMENT#JSDOC#CORE#PARAMS#OPTIONS#LINTER
- * @param options.languageOptions - $COMMENT#JSDOC#CORE#PARAMS#OPTIONS#LANGUAGEOPTIONS
- * @returns $COMMENT#JSDOC#CORE#RETURNS#GETSOURCECODEFROMFILEPATH
+ * Gets the ESLint-generated `SourceCode` object of a file from its absolute path.
+ * @param absolutePath - The absolute path of the file.
+ * @param options - The additional options as follows:
+ * @param options.linter - The `Linter` instance used to retrieve the `SourceCode` object, defaulting to a `new Linter()` per operation ensuring each call of `getSourceCodeFromFilePath` is based on its own linter (just in case somehow some linters were to run concurrently).
+ * @param options.languageOptions - The `languageOptions` object used by `linter.verify()`, defaulting to a version that is TypeScript- and JSX-compatible.
+ * @returns The ESLint-generated `SourceCode` object of the file, from which the AST (`sourceCode.ast`) and all comments (`sourceCode.getAllComments()`) can be extracted. `null` when the file is nonexistent or when encountering fatal syntax.
  */
 export const getSourceCodeFromFilePath = (
   absolutePath: string,
@@ -41,3 +41,7 @@ export const getSourceCodeFromFilePath = (
   if (!sourceCode) return null;
   else return sourceCode;
 };
+
+/* Notes
+Normally I should return a success object, with `success: true` and a provided `sourceCode`, or `success: false` and provided `errors`, explaining the reasons for failures so that I can test them more precisely. But this is a really simple project, and a really simple utility that I use in situations where I'm in complete control of the inputs.
+*/
